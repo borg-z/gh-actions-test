d44684b46ff4491cf9723e47e62bb43f78effe0d
borg-z <me@nikolay-z.top>
2023-03-22 15:05:41 +0300

    test

--- .github/workflows/bump-version.js
+++ .github/workflows/bump-version.js
@@ -1,107 +1,60 @@
-module.exports = ({github, context, versionType, core}) => {
-
-    async function latestRelease() {
-        try {
-            var latestRelease = await github.rest.repos.getLatestRelease({
-                owner: context.repo.owner,
-                repo: context.repo.repo
-            });
-        } catch (error) {
-            if (error.message === 'Not Found') {
-                return '0.0.0';
-            } else {
-                core.setFailed(error.message)
-            }
-        }
-        console.log(latestRelease.data.tag_name)
-        return latestRelease.data.tag_name
+module.exports = async ({github, context, versionType, core}) => {
+    try {
+      // Get the latest release
+      const latestRelease = await github.rest.repos.getLatestRelease({
+        owner: context.repo.owner,
+        repo: context.repo.repo
+      });
+  
+      // Extract the latest semantic version
+      const latestSemanticVersion = latestRelease.data.tag_name;
+      console.log(`Latest Semantic Version: ${latestSemanticVersion}`);
+  
+      // Bump the semantic version based on the version type
+      let [major, minor, patch] = latestSemanticVersion.split(".");
+      switch (versionType) {
+        case "MAJOR":
+          major++;
+          minor = 0;
+          patch = 0;
+          break;
+        case "MINOR":
+          minor++;
+          patch = 0;
+          break;
+        case "PATCH":
+          patch++;
+          break;
+        default:
+          throw new Error("Invalid version type. Must be 'MAJOR', 'MINOR', or 'PATCH'.");
+      }
+      const newSemanticVersion = `${major}.${minor}.${patch}`;
+      console.log(`Bumped Semantic Version: ${newSemanticVersion}`);
+  
+      // Create a new branch and tag for the new semantic version
+      const branchName = `release/${newSemanticVersion}`;
+      await github.rest.git.createRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: `refs/heads/${branchName}`,
+        sha: context.sha
+      });
+      console.log(`Created branch: ${branchName}`);
+  
+      const tagName = newSemanticVersion;
+      await github.rest.git.createRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: `refs/tags/${tagName}`,
+        sha: context.sha
+      });
+      console.log(`Created tag: ${tagName}`);
+  
+      // Set the outputs
+      core.setOutput("semanticVersion", latestSemanticVersion);
+      core.setOutput("newSemanticVersion", newSemanticVersion);
+    } catch (error) {
+      core.setFailed(error.message);
     }
-
-    async function bumpVersion(semanticVersion, versionType) {
-        try {
-            console.log(`versionType: ${versionType}`)
-            let majorNumber = semanticVersion.toString().split(".")[0];
-            let minorNumber = semanticVersion.toString().split(".")[1];
-            let patchNumber = semanticVersion.toString().split(".")[2];
-
-            let newSemanticVersion = '';
-
-            switch (versionType) {
-                case 'MAJOR':
-                    majorNumber = parseInt(majorNumber, 10) + 1;
-                    newSemanticVersion = majorNumber + ".0.0";
-                    break;
-                case 'MINOR':
-                    minorNumber = parseInt(minorNumber, 10) + 1;
-                    newSemanticVersion = majorNumber + "." + minorNumber + ".0"
-                    break;
-                case 'PATCH':
-                    patchNumber = parseInt(patchNumber, 10) + 1;
-                    newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
-
-                    break;
-                default:
-                    core.setFailed("Could not process the version type. Make sure your string is either 'MAJOR', 'MINOR', or 'PATCH'.");
-            }
-            console.log('\x1b[32m%s\x1b[0m', `Bumped Semantic Version: ${newSemanticVersion}`);
-            return newSemanticVersion
-        } catch (error) {
-            core.setFailed(error.message);
-        }
-    }
-
-    async function createBranch(branch) {
-        let branchExists;
-        try {
-            await github.rest.repos.getBranch({
-                ...context.repo,
-                branch
-            })
-
-            branchExists = true;
-        } catch (error) {
-            if (error.name === 'HttpError' && error.status === 404) {
-                await github.rest.git.createRef({
-                    ref: `refs/heads/${branch}`,
-                    sha: context.sha,
-                    ...context.repo
-                })
-            } else {
-                throw Error(error)
-            }
-        }
-    }
-
-    async function createTag(semanticVersion) {
-
-        try {
-            await github.rest.git.getRef({
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                ref: `tags/${semanticVersion}`,
-            });
-
-        } catch (error) {
-            if (error.name === 'HttpError' && error.status === 404) {
-                await github.rest.git.createRef({
-                    owner: context.repo.owner,
-                    repo: context.repo.repo,
-                    ref: `refs/tags/${semanticVersion}`,
-                    sha: context.sha
-                })
-            } else {
-                throw Error(error)
-            }
-        }
-    }
-
-    async function main() {
-        const semanticVersion = await latestRelease()
-        const newSemanticVersion = await bumpVersion(semanticVersion, versionType)
-        createBranch(`release/${newSemanticVersion}`)
-        await createTag(newSemanticVersion)
-        core.setOutput('semanticVersion', semanticVersion);
-        core.setOutput('newSemanticVersion', newSemanticVersion);
-    }
-    main()
-    }
+  };
+  
